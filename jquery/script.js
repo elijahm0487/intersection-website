bb2b9f4cfda663618e44ce1f9dc36c737199f105
Travis Matos <travism7618@hstat.org>
2018-05-09 15:27:51 +0000

    fixes js animation issues, bootstrap changes to bio and navbar jump issues (yet again)

--- jquery/script.js
+++ jquery/script.js
@@ -1,14 +1,19 @@
  $(function() {
             $(".hover").mouseenter(function() {
+                $(this).children(":first").finish()
                 var fontSize = parseInt($(this).children(":first").css("font-size"));
-                console.log(fontSize)
+                console.log("font size before:"+fontSize)
                 fontSize = fontSize + 10 + "px";
                 $(this).children(":first").animate({
                     fontSize: fontSize
+
                 })
+                console.log("font size after:"+fontSize)
+
 
             })
             $(".hover").mouseleave(function() {
+                $(this).children(":first").finish()
                 var fontSize = parseInt($(this).children(":first").css("font-size"));
                 console.log(fontSize)
                 fontSize = fontSize - 10 + "px";
